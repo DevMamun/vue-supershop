78404202cb056e73f48409542cb824187dc879ef
Mamunur Rashid <devmamun8@gmail.com>
2020-08-21 00:07:28 +0600

    Product Add to Cart Done

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,17 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">SuperStore</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+    <navbar></navbar>
     <br>
     <div class="container">
       <div class="row">
       <div class="col-md-9">
         <div class="main-content">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card">
-                <img src="https://picsum.photos/286/180" class="card-img-top" >
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card">
-                <img src="https://picsum.photos/286/180" class="card-img-top" >
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card">
-                <img src="https://picsum.photos/286/180" class="card-img-top" >
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-        </div>
+          <inventory @newItemAdded="addCartItem" :items="items" ></inventory>
         </div>
       </div>
       <div class="col-md-3">
         <div class="aside">
-          <ul class="list-group">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-          </ul>
+          <cart :items="cart"></cart>
         </div>
       </div>
     </div>
@@ -69,9 +20,32 @@
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Inventory from './components/Inventory'
+import data from './data'
 
-
-export default {}
+export default {
+  components : {
+    Navbar,
+    Cart,
+    Inventory
+  },
+  data(){
+    return{
+      items : [],
+      cart : [],
+    }
+  },
+  mounted(){
+    this.items = data;
+  },
+  methods : {
+    addCartItem(item){
+      this.cart.push(item);
+    }
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col-md-4" v-for="(item, id ) in items" :key="id">
+          <div class="card mb-3">
+            <!-- <img :src="item.photo" class="card-img-top" > -->
+            <div class="card-body">
+              <h5 class="card-title">{{ item.title }}</h5>
+              <p class="card-text">${{ item.price }}</p>
+              <a href="#" @click.prevent="addCartItem(item)" class="btn btn-primary">+ Add</a>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props : ['items'],
+    methods : {
+    addCartItem(item){
+      this.$emit('newItemAdded', item);
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
