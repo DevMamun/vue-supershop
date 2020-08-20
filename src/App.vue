af48dce84d8300934f060922123a89ecdec05eb9
Mamunur Rashid <devmamun8@gmail.com>
2020-08-21 05:06:12 +0600

    Featch Data Forn Api Done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
+    <!-- <router-view></router-view> -->
     <navbar @search="search"></navbar>
     <br>
     <div class="container">
       <div class="row">
       <div class="col-md-9">
         <div class="main-content">
-          <inventory @newItemAdded="addCartItem" :items="items" ></inventory>
+          <router-view></router-view>
+          <!-- <inventory @newItemAdded="addCartItem" :items="items" ></inventory> -->
         </div>
       </div>
       <div class="col-md-3">
@@ -22,14 +24,13 @@
 <script>
 import Navbar from './components/Navbar'
 import Cart from './components/Cart'
-import Inventory from './components/Inventory'
+// import Inventory from './components/Inventory'
 import data from './data'
 
 export default {
   components : {
     Navbar,
-    Cart,
-    Inventory
+    Cart
   },
   data(){
     return{
--- src/components/views/Inventory.vue
+++ src/components/views/Inventory.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row">
+    <div v-if="!loading" class="row">
         <div class="col-md-4" v-for="(item, id ) in items" :key="id">
           <div class="card mb-3">
             <!-- <img :src="item.photo" class="card-img-top" > -->
@@ -11,15 +11,36 @@
           </div>
         </div>
     </div>
+    <h1 v-else>Loading...</h1>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  props : ['items'],
-    methods : {
+  data(){
+    return{
+      loading : true,
+      items : []
+    }
+  },
+  methods : {
     addCartItem(item){
       this.$emit('newItemAdded', item);
+    },
+    fatchInventory(){
+      let self = this
+      axios.get('http://localhost:3000/items')
+      .then(res => {
+        setTimeout( function(){
+           self.items = res.data
+            self.loading = false
+        }, 2000);
+
+      });
     }
+  },
+  created(){
+    this.fatchInventory();
   }
 }
 
